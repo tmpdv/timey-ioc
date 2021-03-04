d0ef73ec3ff844f1381eb2ad7b4ec36dbd0caf78
Timofey_Podvorny <Timofey_Podvorny@epam.com>
2021-03-04 17:35:12 +0300

    Hardcoded default configurators

--- src/main/java/timey/ioc/ioc/factory/BeanFactory.java
+++ src/main/java/timey/ioc/ioc/factory/BeanFactory.java
@@ -3,21 +3,33 @@ package timey.ioc.ioc.factory;
 import lombok.SneakyThrows;
 import timey.ioc.ioc.ApplicationContext;
 import timey.ioc.ioc.configurator.BeanConfigurator;
+import timey.ioc.ioc.configurator.FieldBeanInjector;
+import timey.ioc.ioc.configurator.FieldValueInjector;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BeanFactory {
+
+    private static final Set<Class<? extends BeanConfigurator>> DEFAULT_BEAN_CONFIGURATOR_CLASSES = Set.of(
+            FieldBeanInjector.class, FieldValueInjector.class);
 
     private final List<BeanConfigurator> configurators = new ArrayList<>();
 
     private final ApplicationContext context;
 
+
     @SneakyThrows
     public BeanFactory(ApplicationContext context) {
         this.context = context;
-        for (Class<? extends BeanConfigurator> clazz :
-                context.getConfig().getScanner().getSubTypesOf(BeanConfigurator.class)) {
+        // Adding default configurators
+        Set<Class<? extends BeanConfigurator>> beanConfiguratorClasses = new HashSet<>(DEFAULT_BEAN_CONFIGURATOR_CLASSES);
+        // Adding custom configurators
+        beanConfiguratorClasses.addAll(context.getConfig().getScanner().getSubTypesOf(BeanConfigurator.class));
+
+        for (Class<? extends BeanConfigurator> clazz : beanConfiguratorClasses) {
             this.configurators.add(clazz.getDeclaredConstructor().newInstance());
         }
     }
